ca8f71300e79b1e87dc08a44ab05d029cf485aed
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-14 07:51:24 +0300

    auto commit

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -36,12 +36,3 @@ function cheatsheet_list() {
   echo $values
 }
 
-
-cs-help() {
-    echo "\n${COLOR_YELLOW}  Cheatsheet:";
-    echo "${COLOR_BLUE}  cs <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name>"
-    echo "${COLOR_BLUE}  csbat <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using bat app"
-    echo "${COLOR_BLUE}  csglow <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using glow app"
-    echo "${COLOR_BLUE}  csless <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using less app"
-    echo "${COLOR_BLUE}  csl ${COLOR_GREEN}- display the list of cheat sheets that can be searched for"
-}
