ec8654598c9d2f6ea3502fe30236ac1469baa708
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-14 07:06:02 +0300

    Auto git push ...

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -3,7 +3,6 @@ url='https://github.com'
 raw_url='https://raw.githubusercontent.com'
 main_src='main/src'
 
-
 function cheatsheet() {
   local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
 
@@ -15,19 +14,19 @@ function cheatsheet() {
       echo "$value" | bat -l 'markdown' -n
     fi
   fi
-
 }
 
 function cheatsheet_list() {
-  local value=$(
-    curl -s "${url}/${repo_name}/tree/${main_src}" | \
-    grep '>.*\.md<'                                | \
-    sed -e 's/<[^>]*>//g'                          | \
-    sed -e 's/\.md//g'                             | \
-    awk '{print $1}'
-  )
-  echo "$value"
+  local values=$(curl -s "${raw_url}/${repo_name}/main/content.md")
+  echo $values
 }
 
 alias cs="cheatsheet"
 alias csl="cheatsheet_list"
+
+
+cs-help() {
+    echo "\n${COLOR_YELLOW}  Cheatsheet:";
+    echo "${COLOR_BLUE}  cs <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name>"
+    echo "${COLOR_BLUE}  csl ${COLOR_GREEN}- display the list of cheat sheets that can be searched for"
+}
