68c6556472fce9836eae8d61f08d2a477f648a5c
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-16 12:03:13 +0300

    auto commit

--- list-gacp.sh
+++ list-gacp.sh
@@ -1,4 +1,5 @@
-tree src | sed 1d | sed '$d' > content.md
+# tree src | sed 1d | sed '$d' > content.md
+ls -l src | awk '{print $9}' | sed 's/.md//g' | nl 
 git add .
 git commit -a -m "auto commit"
 git push
